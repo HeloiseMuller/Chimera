8b56d55906f1e7380b912aa252caca32a6c4c17b
Héloïse Muller <hmuller@localhost.localdomain>
2022-03-02 10:17:51 +0100

    improve second figure

--- src/1-Coverage.R
+++ src/1-Coverage.R
@@ -37,7 +37,6 @@ source(paste0(scriptPath, "/functions.Rdata"))
 cov = lapply(paste0(depth_dir, samples, "_CtBV.cov"),fread)
 
 
-
 ### Main ###
 
 png(paste0(dir,"/Figures/Coverage.png"),  width = 8, height = 5, units = 'in', res = 600)    
@@ -69,20 +68,24 @@ for (i in seq_along(cov)){
     cov[[i]]$Sample = samples[i]
     }
 
-cov_seg = mutate(cov[[1]],relativeDepth = Depth/Coverage[1,]$depth2) %>% select(., Segment, relativeDepth, Seg, Sample) 
+#Relatice depth. Here, one table with all depths
+cov_seg = mutate(cov[[1]], relativeDepth = Depth-Coverage[1,]$depth2) %>% select(., Segment, relativeDepth, Seg, Sample) 
 for (i in 2:length(cov)){
-    cov_seg = rbind(cov_seg, mutate(cov[[i]],relativeDepth = Depth/Coverage[i,]$depth2) %>% select(., Segment, relativeDepth, Seg, Sample) )
+    cov_seg = rbind(cov_seg, mutate(cov[[i]], relativeDepth = Depth-Coverage[i,]$depth2) %>% select(., Segment, relativeDepth, Seg, Sample) )
     }
- 
+cov_seg = mutate(cov_seg, relativeDepth = ifelse(relativeDepth<0, 0, relativeDepth))
+print(filter(cov_seg, Sample=="NH212C"))
+
 png(paste0(dir,"/Figures/Coverage_segments.png"),  width = 10, height = 5, units = 'in', res = 600)     
-dotchart(cov[[1]]$Depth/Coverage[1,]$depth2, 
+par(mar=c(4, 0, 1, 6), xpd=TRUE)
+dotchart(cov[[1]]$Depth-Coverage[1,]$depth2, 
     labels= cov[[1]]$Segment, cex = 0.8,
     bg = colors[1], pt.cex = 0.8, 
-    xlim = c(0,ceiling(max(cov_seg$relativeDepth))*1.5), 
-    xlab = expression(paste("Segment depths relatively to the depth of ", italic('C. typhae'))))
+    xlim = c(0, ceiling(max(cov_seg$relativeDepth))*1.5), 
+    xlab = expression(paste("Segment depths minus the depth on ", italic('C. typhae'))))
 for (i in 2:length(cov)){
-    points(cov[[i]]$Depth, 1:nrow(cov[[1]]), col = colors[i], pch = 19, cex = 0.8)
+    points(cov[[i]]$Depth-Coverage[i,]$depth2, 1:nrow(cov[[1]]), col = colors[i], pch = 19, cex = 0.8)
 }
-legend(90, y=17, legend = names, col = colors, fill = colors, pch=19, cex = 0.8)
+legend("topright", inset=c(-0.15, .2), legend = names, col = colors, fill = colors, pch=19, cex = 0.8)
 dev.off()
 
